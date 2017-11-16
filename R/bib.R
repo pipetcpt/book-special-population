aa42f5071120b8048264da9e90f9466e8e4175f3
Sungpil Han CPT <shan@acp.kr>
2017-11-17 08:46:57 +0900

    publish

--- R/bib.R
+++ R/bib.R
@@ -1,6 +1,8 @@
 library(knitcitations)
 cleanbib()
 options("citation_format" = "pandoc")
+citep('10.1038/psp.2012.2')
+citep('10.1124/dmd.112.050161')
 citep('10.1038/clpt.2012.19') # regulatory - ped drug
 citep('10.2174/1568026617666170403113541')
 citep('10.1124/dmd.116.069559')
